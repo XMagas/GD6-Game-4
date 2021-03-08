9df008695e02c0c13a05053953236e851920b602
Andreas Hansen <andreashansen513@outlook.com>
2021-03-08 13:28:46 +0100

    Project,Movement and Projectile

---
+++ GD6 Game 4/Assets/Player/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = -5;
+
+    public Rigidbody2D rb;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb.velocity = transform.up * speed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GD6 Game 4/Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 4;
+
+
+    public GameObject RainBullet;
+
+    public Transform RainPosition;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var movement = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3 (movement, 0, 0) * Time.deltaTime * speed;
+
+
+        if (Input.GetButton("Fire1"))
+        {
+
+            Rain();
+
+
+
+
+        }
+
+
+
+    }
+
+
+    void Rain()
+    {
+
+
+
+
+        Instantiate(RainBullet,RainPosition.position, RainPosition.rotation);
+
+
+
+
+
+    }
+
+
+
+
+
+}
